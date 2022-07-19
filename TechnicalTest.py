68a9b0f6fc6413e6ab4f6d44f14293da81c85de1
eve-bri <65525208+eve-bri@users.noreply.github.com>
2022-07-19 11:34:35 -0600

    Try to convert to Pythonic code

--- TechnicalTest.py
+++ TechnicalTest.py
@@ -1,10 +1,22 @@
 def main():
-    #strArr =  ["baseball", "a,all,b,ball,bas,base,cat,code,d,e,quit,z"]
-    strArr =  ["abcgefd", "a,ab,abc,abcg,b,c,dog,e,efd,zzzz"]
+    strArr =  ["baseball", "a,all,b,ball,bas,base,cat,code,d,e,quit,z"]
+    #strArr =  ["abcgefd", "a,ab,abc,abcg,b,c,dog,e,efd,zzzz"]
     strArr[0] = strArr[0].lower()
     strArr[1] = strArr[1].lower()
-    print(WordSplit(strArr))
+    print("Resultado: " + WordSplit(strArr))
 
+def WordSplit(strArr):
+    outPut = "Not possible"
+    dictionaryWord = strArr[1].split(',')
+    for index in range(len(strArr[0])):
+        if (strArr[0][:index] in dictionaryWord and 
+            strArr[0][index:] in dictionaryWord):
+            outPut = strArr[0][:index] + "," + strArr[0][index:]
+            break
+    return outPut
+
+'''
+#Antes
 def WordSplit(strArr):
     contIndex = 1
     return RecursiveWordSplit(strArr,contIndex)
@@ -17,7 +29,7 @@ def RecursiveWordSplit(strArr,contIndex):
             return RecursiveWordSplit(strArr,contIndex + 1)
     else:
         return "not possible"
-
+'''
 if __name__ == '__main__':
     import sys
     sys.exit(int (main() or 0))
